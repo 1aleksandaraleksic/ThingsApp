0366057e32d4a02dcb7b72f029ee0854a1352830
Aleksandar Aleksic <1aleksandaraleksic@gmail.com>
2023-06-07 22:35:57 +0200

    Add new missing file DeviceScreen

---
+++ ThingsApp/Utils/DeviceScreen.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceScreen.swift
+//  ThingsApp
+//
+//  Created by aleksandar.aleksic on 7.6.23..
+//
+
+import UIKit
+
+class DeviceScreen {
+    public static var width = UIScreen.main.bounds.width
+    public static var height = UIScreen.main.bounds.height
+}
